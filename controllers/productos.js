c73c656cd127f1f1cdc43d6ae15fe2a78b3fc868
CarlosPaz64 <134614533+CarlosPaz64@users.noreply.github.com>
2024-03-13 19:19:18 -0600

    Cambios varios

--- controllers/productos.js
+++ controllers/productos.js
@@ -6,7 +6,12 @@ const productos = [
     { id: 4, nombre: 'Super Mario Party', descripcion: 'Descripción del Producto 4', cantidad: 10, precio: 750, imagen: 'producto4.jpg' },
     { id: 5, nombre: 'Super Smash Bros Ultimate', descripcion: 'Descripción del Producto 5', cantidad: 15, precio: 450, imagen: 'producto5.jpg' },
     { id: 6, nombre: 'Red Dead Redemption', descripcion: 'Descripción del Producto 6', cantidad: 5, precio: 350, imagen: 'producto6.jpg' },
-    { id: 7, nombre: 'Red Dead Redemption 2', descripcion: 'Descripción del Producto 7', cantidad: 5, precio: 890, imagen: 'producto7.jpg' }
+    { id: 7, nombre: 'Red Dead Redemption 2', descripcion: 'Descripción del Producto 7', cantidad: 5, precio: 890, imagen: 'producto7.jpg' },
+    { id: 8, nombre: 'Fallout 4: Game Of The Year', descripcion: 'Descripción del Producto 7', cantidad: 5, precio: 500, imagen: 'producto8.jpg' },
+    { id: 9, nombre: 'Jurassic World Evolution', descripcion: 'Descripción del Producto 7', cantidad: 5, precio: 350, imagen: 'producto9.jpg' },
+    { id: 10, nombre: 'Metroid: Dread', descripcion: 'Descripción del Producto 7', cantidad: 5, precio: 400, imagen: 'producto10.jpg' },
+    { id: 11, nombre: 'Grand Theft Auto 5', descripcion: 'Descripción del Producto 7', cantidad: 5, precio: 200, imagen: 'producto11.jpg' },
+    { id: 12, nombre: 'EA Sports FC 24', descripcion: 'Descripción del Producto 7', cantidad: 5, precio: 600, imagen: 'producto12.jpg' }
   ];
   
   function getProductos() {
